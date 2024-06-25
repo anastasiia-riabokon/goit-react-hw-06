20a549bcf01e2ee8f241ac4ee72fa1f3dc01dac1
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-25 17:01:54 +0300

    refactor: deleteContact

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,22 +1,20 @@
+import {useDispatch} from "react-redux";
 import css from "./SearchBox.module.css";
 import {useId} from "react";
+import {changeFilter} from "../../redux/filtersSlice";
 
-export const SearchBox = ({searchValue, onSearchValue}) => {
+export const SearchBox = () => {
+  const dispatch = useDispatch();
   const idSearch = useId();
   return (
     <label className={css.search__block} htmlFor={idSearch}>
-      <span className={css.label}>
-        Find contacts by name
-      </span>
+      <span className={css.label}>Find contacts by name</span>
       <input
         className={css.field}
         type="search"
         id={idSearch}
         placeholder="Search..."
-        value={searchValue}
-        onChange={(event) =>
-          onSearchValue(event.target.value)
-        }
+        onChange={(event) => dispatch(changeFilter(event.target.value))}
       />
     </label>
   );
