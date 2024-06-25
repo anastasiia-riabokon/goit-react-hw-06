344fffea0917434cb0d279f66586fa5d92d8a8fb
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-25 17:04:25 +0300

    refactor: removing unnecessary props

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,15 +1,12 @@
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
-export const ContactList = ({users, deleteContact}) => {
+export const ContactList = ({users}) => {
   return (
     <ul className={css.list}>
       {users.map((user) => (
         <li className={css.item} key={user.id}>
-          <Contact
-            values={user}
-            deleteContact={deleteContact}
-          />
+          <Contact values={user} />
         </li>
       ))}
     </ul>
