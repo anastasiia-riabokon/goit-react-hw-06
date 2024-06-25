ed9b80a3a2208cc97b12d52cca740f54c288d010
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-25 15:57:53 +0300

    feat: add provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "modern-normalize";
 import "./index.css";
+import {store} from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
