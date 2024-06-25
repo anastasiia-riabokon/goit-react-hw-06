cb3151d6e21cd785357b1cc94211819a53b8e7e1
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-06-25 15:00:41 +0300

    feat: create slice contact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,8 +1,27 @@
+import {createSlice} from "@reduxjs/toolkit";
+
 const initialState = {
   contacts: {
     items: [],
   },
-  filters: {
-    name: "",
-  },
 };
+
+const slice = createSlice({
+  name: "contact",
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.items.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      state.contacts.items.filter((item) => item.id !== action.payload);
+    },
+  },
+  selectors: {
+    selectContacts: (state) => state.contacts,
+  },
+});
+
+export const contactReducer = slice.reducer;
+export const {addContact, deleteContact} = slice.actions;
+export const {selectContacts} = slice.selectors;
